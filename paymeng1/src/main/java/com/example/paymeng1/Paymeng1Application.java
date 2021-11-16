fd4d3d82652f63b1e705b8baeb9ecf7fa92ff472
zqf <1936534366@qq.com>
2021-11-16 17:11:43 +0800

    Initial commit

---
+++ paymeng1/src/main/java/com/example/paymeng1/Paymeng1Application.java
@@ -0,0 +1,13 @@
+package com.example.paymeng1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Paymeng1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Paymeng1Application.class, args);
+    }
+
+}
