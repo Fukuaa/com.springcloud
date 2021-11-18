39647c79a12d98069a0288b1edf515ab7eaec974
zqf <1936534366@qq.com>
2021-11-18 16:58:20 +0800

    consul

---
+++ order/src/main/java/com/example/order/entities/commonresult.java
@@ -0,0 +1,17 @@
+package com.example.order.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class commonresult<t> {
+    private Integer code;
+    private String message;
+    private t data;
+    public commonresult(Integer code,String message){
+        this(code,message,null);
+    }
+}
