d0496ae0fa4c72f3a2645b6e9a669376c2eb0872
zqf <1936534366@qq.com>
2021-11-19 22:24:09 +0800

    修改

---
+++ hystrix/src/main/java/com/example/hystrix/services/simpl.java
@@ -0,0 +1,13 @@
+package com.example.hystrix.services;
+
+public class simpl implements s{
+    @Override
+    public String p(Integer id) {
+        return null;
+    }
+
+    @Override
+    public String p1(Integer id) {
+        return null;
+    }
+}
