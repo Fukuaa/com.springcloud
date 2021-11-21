d894363edd2605653373e043256dd4e79d9d0d86
zqf <1936534366@qq.com>
2021-11-21 19:15:44 +0800

    config

--- config/src/main/java/com/example/config/ConfigApplication.java
+++ config/src/main/java/com/example/config/ConfigApplication.java
@@ -1,15 +1,15 @@
-package com.example.configcenter;
+package com.example.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-@SpringBootApplication
 @EnableConfigServer
-public class ConfiGcenterApplication {
+@SpringBootApplication
+public class ConfigApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConfiGcenterApplication.class, args);
+        SpringApplication.run(ConfigApplication.class, args);
     }
 
 }
--- config/src/test/java/com/example/config/ConfigApplicationTests.java
+++ config/src/test/java/com/example/config/ConfigApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.configcenter;
+package com.example.config;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ConfiGcenterApplicationTests {
+class ConfigApplicationTests {
 
     @Test
     void contextLoads() {
