45409f2d9cb9b0662e1fa21c9b8a1f64334ceabd
zqf <1936534366@qq.com>
2021-11-21 14:51:43 +0800

    gateway

---
+++ hystrixdashbord/src/main/java/com/example/hystrixdashbord/HystrixdashbordApplication.java
@@ -0,0 +1,13 @@
+package com.example.hystrixdashbord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HystrixdashbordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixdashbordApplication.class, args);
+    }
+
+}
