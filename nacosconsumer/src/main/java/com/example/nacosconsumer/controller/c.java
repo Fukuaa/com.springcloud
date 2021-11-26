2c17f8c5c067b87d5b660d196d2aa791c9976217
zqf <1936534366@qq.com>
2021-11-26 22:23:42 +0800

    sentinle

--- nacosconsumer/src/main/java/com/example/nacosconsumer/controller/c.java
+++ nacosconsumer/src/main/java/com/example/nacosconsumer/controller/c.java
@@ -1,5 +1,7 @@
 package com.example.nacosconsumer.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,9 +17,23 @@ public class c {
     private RestTemplate restTemplate;
     @Value("${service-url.nacos-user-service}")
     private String string;
+    @SentinelResource(value = "fallback",fallback = "hamdlerfallback",blockHandler = "blockHandler")
+    //@SentinelResource(value = "fallback",fallback = "hamdlerfallback")
     @RequestMapping(value = "/n/{str}", method = RequestMethod.GET)
     public String echo(@PathVariable String str) {
-        return restTemplate.getForObject(string +"/n/"+str, String.class);
+        if (str.equals("4")){
+            throw new IllegalArgumentException("非法参数异常");
+        }else if (str.equals("5")){
+            throw new NullPointerException("空指针异常");
+        }else {
+            return restTemplate.getForObject(string + "/n/" + str, String.class);
+        }
+    }
+    public String hamdlerfallback(@PathVariable String s,Throwable throwable){
+        return "💕💕💕"+s+throwable.getMessage();
+    }
+    public String blockHandler(@PathVariable String s, BlockException blockException){
+        return "💕💕💕"+s+blockException.getMessage();
     }
 }
 
