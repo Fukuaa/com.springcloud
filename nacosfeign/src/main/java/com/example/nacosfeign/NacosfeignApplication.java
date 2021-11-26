9bd90bb25cd30fb3708adfcb5a7629f5feba1c5f
zqf <1936534366@qq.com>
2021-11-27 00:57:15 +0800

    sentinle

---
+++ nacosfeign/src/main/java/com/example/nacosfeign/NacosfeignApplication.java
@@ -0,0 +1,13 @@
+package com.example.nacosfeign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosfeignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosfeignApplication.class, args);
+    }
+
+}
---
+++ nacosfeign/src/test/java/com/example/nacosfeign/NacosfeignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nacosfeign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NacosfeignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
